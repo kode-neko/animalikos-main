f0dfef0a4c8e12277d4ba1671e20424c22ceaff0
Kodeneko <ladysun.freedom@gmail.com>
2023-09-21 21:24:24 +0200

    [main-bar] change color theme

--- src/screen/main/main.tsx
+++ src/screen/main/main.tsx
@@ -4,17 +4,19 @@ import { Color, Contact } from '../../model';
 import styles from './styles.module.less';
 import { getContact } from '../../service/getters';
 import classNames from 'classnames';
+import { useAtom } from 'jotai';
+import { appColor } from '../../store';
 
 const Main: Preact.FunctionComponent = () => {
 
+  const [themeColor, setThemeColor] = useAtom(appColor);
   const initMsg: Contact = {
     name: '',
     mail: '',
     content: ''
   };
-  const handleSelectColor: (c: Color) => void = (c: Color) => {
-    console.log(c);
-  };
+  const handleSelectColor: (c: Color) => void = (c: Color) =>
+    setThemeColor(c);
   const handleSend: (contact: Contact) => void = (contact: Contact) => {
     getContact().sendMessage(contact)
       .then((res: Contact) => console.log('mensaje enviado: ', res))
@@ -22,15 +24,17 @@ const Main: Preact.FunctionComponent = () => {
   };
 
   return (
-    <div className={classNames(styles.cont, 'blue')}>
-      <MainBar onSelectColor={handleSelectColor} />
-      <Section title="Projects">
-        <Projects />
-      </Section>
-      <Section title="Contact">
-        <FormContact contact={initMsg} onSend={handleSend}/>
-      </Section>
-      <Footer label="Kodeneko @ 2023" />
+    <div className={classNames(styles.outer, themeColor)}>
+      <div className={styles.cont}>
+        <MainBar onSelectColor={handleSelectColor} />
+        <Section title="Projects">
+          <Projects />
+        </Section>
+        <Section title="Contact">
+          <FormContact contact={initMsg} onSend={handleSend}/>
+        </Section>
+        <Footer label="Kodeneko @ 2023" />
+      </div>
     </div>
   );
 };
--- src/store/app.ts
+++ src/store/app.ts
@@ -1,6 +1,7 @@
 import {atom, PrimitiveAtom} from 'jotai';
+import { Color } from '../model';
 
-const appColor: PrimitiveAtom<string> = atom('');
+const appColor: PrimitiveAtom<string> = atom(Color.BLUE as string);
 const appLoading: PrimitiveAtom<boolean> = atom(false);
 const appLang: PrimitiveAtom<string> = atom('en');
 
